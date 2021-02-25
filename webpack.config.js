754cf904ef95f1717f25e0919cdc906e501df4a0
Ryan French <ryanfrench@outlook.com>
2021-02-25 14:40:37 +0000

    prod build changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 // Whilst the configuration object can be modified here, the recommended way of making
 // changes is via the presets' options or Neutrino's API in `.neutrinorc.js` instead.
 // Neutrino's inspect feature can be used to view/export the generated configuration.
-const neutrino = require('neutrino');
+const neutrino = require("neutrino");
 
-module.exports = neutrino().webpack();
+const config = neutrino().webpack();
+
+module.exports = {
+  ...config,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+};
